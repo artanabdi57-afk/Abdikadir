6990430c4fbf0a90df03cc574c2cc27e73b0db00
artanabdi57-afk <artanabdi57@gmail.com>
2026-08-15 20:28:50 +0300

    Implement routing with BrowserRouter and Routes

--- sahan-app/src/main.jsx
+++ sahan-app/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import CertificateBuilder from './pages/CertificateBuilder.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/certificate-builder" element={<CertificateBuilder />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
