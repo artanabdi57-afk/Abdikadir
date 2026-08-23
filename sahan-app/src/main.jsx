40e9d8e74a9403d9f1e95da841d19ee174b3a6e3
artanabdi57-afk <artanabdi57@gmail.com>
2026-08-23 23:30:21 +0300

    Keep existing learning dashboard accessible at /app while using new Sahan public experience as default

--- sahan-app/src/main.jsx
+++ sahan-app/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SahanExperience from './sahan/SahanExperience.jsx'
+import LegacyApp from './App.jsx'
 import './styles/index.css'
+
+const Root = () => window.location.pathname.startsWith('/app') ? <LegacyApp /> : <SahanExperience />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SahanExperience />
+    <Root />
   </React.StrictMode>,
 )
